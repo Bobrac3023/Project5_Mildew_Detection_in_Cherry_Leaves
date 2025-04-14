3b2da100463add723b75488b0c00e2a11ec5b12c
Bobrac3023 <roshan@secureworkspace.tech>
2025-04-14 08:51:36 +0000

    configure app pages

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 # load pages scripts
 from app_pages.executive_project_summary import executive_project_summary
-from app_pages.dataset_cells_visualizer import dataset_cells_visualizer
+from app_pages.cherry_leaves_visualizer import cherry_leaves_visualizer
 from app_pages.mildew_powdery_detection import mildew_powdery_detection
 from app_pages.project_hypothesis_validation import project_hypothesis_validation
 from app_pages.project_performance_metrics import project_performance_metrics
@@ -12,14 +12,14 @@ st.set_page_config(page_title="Mildew Detection in Cherry Leaves", layout="wide"
 
 # Sidebar navigation
 st.sidebar.title("Navigation Panel")
-options = ["Executive project summary", "Dataset cells visualizer", "Mildew_powdery detection", "Project hypothesis validation", "Project performance metrics"]
+options = ["Executive project summary", "Cherry Leaves visualizer", "Mildew_powdery detection", "Project hypothesis validation", "Project performance metrics"]
 selection = st.sidebar.radio("Select radio button below", options)
 
 # Page display
 if selection == "Executive project summary":
     executive_project_summary()
-elif selection == "Dataset cells visualizer":
-    dataset_cells_visualizer()
+elif selection == "Cherry Leaves visualizer":
+    cherry_leaves_visualizer()
 elif selection == "Mildew_powdery detection":
     mildew_powdery_detection()
 elif selection == "Project hypothesis validation":
--- app_pages/dataset_cells_visualizer.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def dataset_cells_visualizer():
-    st.title("Project Summary: Mildew Detection in Cherry Leaves")
