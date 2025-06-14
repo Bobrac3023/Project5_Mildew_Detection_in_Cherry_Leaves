96352b2d9159578dd3643a3a4fb29df5be131873
Bobrac3023 <roshan@secureworkspace.tech>
2025-06-14 15:20:02 +0400

    reconfigure cherry_leaves_vissualizer.py file

--- app_pages/cherry_leaves_visualizer.py
+++ app_pages/cherry_leaves_visualizer.py
@@ -1,100 +1,132 @@
-
-
-import streamlit as st
 import os
-import pandas as pd
+import random
+import itertools
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
+from PIL import UnidentifiedImageError
 
-import itertools
-import random
+import streamlit as st
+
 
 def cherry_leaves_visualizer():
     st.title("Cherry Leaves Visualizer")
-    st.info(F"Business Requirement 1- Conduct a study to visually differentiate a cherry leaf that is healthy from one that contains powder.")
-    
+    st.info(
+        "Business Requirement 1 - Conduct study to visually differentiate "
+        "cherry leaf that is healthy from one that contains powder."
+    )
+
     version = 'v1'
-    if st.checkbox("Mean and Standard Deviation for Healhty and Mildew Powdery image"):
-      st.info(f"in general mean and standard devition is called average and varibality")
-      
-      avg_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
-      avg_powdery = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
 
-      st.warning(
-        f"* Output Analaysis:- The patterns of average and variability images do not help to intuitively differentiate one from another. " 
-        f"However, both lables display a small difference in the colour pigment of the average images")
+    if st.checkbox("Mean and Standard Deviation for Healthy and Mildew Powdery image"):
+        st.info(
+            "In general, mean and standard deviation represent "
+            "average and variability."
+        )
 
-      st.image(avg_healthy, caption='Healthy Cherry Leaf - Average and Variability')
-      st.image(avg_powdery, caption='Mildew Powdery Cherry Leaf - Average and Variability')
-      st.write("---")
+        avg_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
+        avg_powdery = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
+
+        st.warning(
+            "* Output Analysis: The patterns of average and variability images "
+            "do not help to intuitively differentiate one from another. "
+            "However, both labels display a small difference in the colour "
+            "pigment of the average images."
+        )
+
+        st.image(
+            avg_healthy,
+            caption='Healthy Cherry Leaf - Average and Variability'
+        )
+        st.image(
+            avg_powdery,
+            caption='Mildew Powdery Cherry Leaf - Average and Variability'
+        )
+        st.write("---")
 
     if st.checkbox("Differences between average healthy and average mildew powdery cells"):
-          diff_between_avgs = plt.imread(f"outputs/{version}/difference_between_averages.png")
+        diff_between_avgs = plt.imread(
+            f"outputs/{version}/difference_between_averages.png"
+        )
 
-          st.warning(
-            f"* We notice this study didn't show "
-            f"patterns where we could intuitively differentiate one from another.")
-          st.image(diff_between_avgs, caption='Difference between average images')
+        st.warning(
+            "* We notice this study didn't show patterns where we could "
+            "intuitively differentiate one from another."
+        )
+        st.image(diff_between_avgs, caption='Difference between average images')
 
-    if st.checkbox("Image Montage"): 
-      st.write("* click 'Create Montage' button to refresh*")
-      my_data_dir = 'input/dataset/cherry-leaves'
-      labels = os.listdir(my_data_dir+ '/validation')
-      label_to_display = st.selectbox(label="Select label", options=labels, index=0)
-      if st.button("Create Montage"):      
-        image_montage(dir_path= my_data_dir + '/validation',
-                      label_to_display=label_to_display,
-                      nrows=8, ncols=3, figsize=(10,25))
-      st.write("---")
+    if st.checkbox("Image Montage"):
+        st.write("* Click 'Create Montage' button to refresh *")
+        my_data_dir = 'input/dataset/cherry-leaves'
+        labels = os.listdir(os.path.join(my_data_dir, 'validation'))
+
+        label_to_display = st.selectbox(label="Select label", options=labels, index=0)
+
+        if st.button("Create Montage"):
+            image_montage(
+                dir_path=os.path.join(my_data_dir, 'validation'),
+                label_to_display=label_to_display,
+                nrows=8,
+                ncols=3,
+                figsize=(10, 25)
+            )
+        st.write("---")
 
 
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
+    sns.set_style("white")
+    labels = os.listdir(dir_path)
 
-def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
-  sns.set_style("white")
-  labels = os.listdir(dir_path)
+    if label_to_display in labels:
+        images_list = os.listdir(os.path.join(dir_path, label_to_display))
 
-  # subset the class you are interested to display
-  if label_to_display in labels:
+        if nrows * ncols < len(images_list):
+            img_idx = random.sample(images_list, nrows * ncols)
+        else:
+            print(
+                f"Decrease nrows or ncols to create your montage.\n"
+                f"There are {len(images_list)} in your subset. "
+                f"You requested a montage with {nrows * ncols} spaces."
+            )
+            return
 
-    # checks if your montage space is greater than subset size
-    # how many images in that folder
-    images_list = os.listdir(dir_path+'/'+ label_to_display)
-    if nrows * ncols < len(images_list):
-      img_idx = random.sample(images_list, nrows * ncols)
+        list_rows = range(nrows)
+        list_cols = range(ncols)
+        plot_idx = list(itertools.product(list_rows, list_cols))
+
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+        plotted = 0
+
+        for x in range(nrows * ncols):
+            try:
+                img_path = os.path.join(dir_path, label_to_display, img_idx[x])
+                img = imread(img_path)
+                img_shape = img.shape
+
+                ax = axes[plot_idx[plotted][0], plot_idx[plotted][1]]
+                ax.imshow(img)
+                ax.set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+                ax.set_xticks([])
+                ax.set_yticks([])
+
+                plotted += 1
+
+            except (UnidentifiedImageError, OSError):
+                st.warning(f"Skipping unreadable image: {img_idx[x]}")
+                continue
+
+            if plotted >= nrows * ncols:
+                break
+
+        plt.tight_layout()
+        st.pyplot(fig=fig)
+
     else:
-      print(
-          f"Decrease nrows or ncols to create your montage. \n"
-          f"There are {len(images_list)} in your subset. "
-          f"You requested a montage with {nrows * ncols} spaces")
-      return
-    
+        print("The label you selected doesn't exist.")
+        print(f"The existing options are: {labels}")
 
-    # create list of axes indices based on nrows and ncols
-    list_rows= range(0,nrows)
-    list_cols= range(0,ncols)
-    plot_idx = list(itertools.product(list_rows,list_cols))
-
-
-    # create a Figure and display images
-    fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
-    for x in range(0,nrows*ncols):
-      img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
-      img_shape = img.shape
-      axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
-      axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
-      axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
-      axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
-    plt.tight_layout()
-    
-    st.pyplot(fig=fig)
-    # plt.show()
-
-
-  else:
-    print("The label you selected doesn't exist.")
-    print(f"The existing options are: {labels}")
 
 if __name__ == "__main__":
     cherry_leaves_visualizer()
