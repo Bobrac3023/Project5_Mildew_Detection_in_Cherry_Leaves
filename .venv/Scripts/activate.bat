b7c6c8ac94a91f4ff72c63a99e335b0d549db0c0
Bobrac3023 <roshan@secureworkspace.tech>
2025-06-15 16:42:13 +0400

    recloned the repo from github, rerun data collection and data visualization notebooks

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\vscode-projects\Project5\Project5_Mildew_Detection_in_Cherry_Leaves\.venv
+set VIRTUAL_ENV=c:\Roshan Learning\Project5\Project5_Mildew_Detection_in_Cherry_Leaves\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
